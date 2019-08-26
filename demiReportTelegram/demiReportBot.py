192a16cf91b9cf5e74b83c172946294a665e845d
amadorpires <49063617+amadorpires@users.noreply.github.com>
2019-08-26 10:24:24 +0200

    Daily Reward added
    Added a new function that gives everyone in the group one extra point for the ranking.

--- demiReportTelegram/demiReportBot.py
+++ demiReportTelegram/demiReportBot.py
@@ -460,6 +460,9 @@ def login_account(bot, job):
 def pole_timer(job_queue):
     x = datetime.datetime.today()
     y = x.replace(day=x.day, hour=23, minute=59, second=55, microsecond=0)
+    # Para desarrollo usar el import timedelta y la variable 'y' de abajo, para producción la de arriba
+    #from datetime import timedelta
+    #y = x + timedelta(seconds=10)
     y2 = x.replace(day=x.day, hour=1, minute=00, second=00, microsecond=0) + datetime.timedelta(days=1)
     delta_t = y - x
     delta_t2 = y2 - x
@@ -467,6 +470,7 @@ def pole_timer(job_queue):
     secs2 = delta_t2.seconds + 1
     job_queue.run_daily(callback=demi_utils.pole_counter, time=secs)
     job_queue.run_daily(callback=poles.run_daily_perros, time=secs-20)
+    job_queue.run_daily(callback=poles.daily_reward, time=secs+60)
     job_queue.run_daily(callback=variables.clean_poles, time=secs2)
     job_queue.run_repeating(callback=login_account, interval=datetime.timedelta(days=30), first=5)
 
